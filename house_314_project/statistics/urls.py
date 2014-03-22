e6c4f17140b8ddf2f6b962dc90274665395e588c
Vlad Iulian Schnakovszki <shnako@yahoo.com>
2014-03-22 17:39:24 +0000

    Removed url mappings for deleted functions.

--- house_314_project/statistics/urls.py
+++ house_314_project/statistics/urls.py
@@ -4,7 +4,4 @@ from statistics import api
 
 urlpatterns = patterns('',
     url(r'^get_current_consumption/$', api.get_current_consumption, name='get_current_consumption'),
-    url(r'^get_average_consumption/$', api.get_average_consumption, name='get_average_consumption'),
-    url(r'^get_area_average_consumption/$', api.get_area_average_consumption, name='get_area_average_consumption'),
-    url(r'^get_city_average_consumption/$', api.get_city_average_consumption, name='get_city_average_consumption'),
 )
